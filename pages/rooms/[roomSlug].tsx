b69ab864fc39ecffb622a72d398c79f188c85030
lalonsoch <lalonso@character.xyz>
2022-05-07 16:12:35 -0300

    .

--- pages/rooms/[roomSlug].tsx
+++ pages/rooms/[roomSlug].tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import axios from 'axios';
-import React from 'react'
 // Components
 import { Layout, Header, CheckAvailabilty, SingleRoomIntro, SingleRoomInfo, RelatedRooms } from '../../components';
 // Utils
