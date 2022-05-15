adc3390460820b6084852ec838df23910702c175
LucasAlonso11800 <lucasalonso11800@gmail.com>
2022-05-14 21:45:47 -0300

    log

--- pages/api/get-page-image.ts
+++ pages/api/get-page-image.ts
@@ -9,7 +9,9 @@ import type { PageImageType } from '../../types';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const { page } = req.body;
+        console.log(page)
         const image: PageImageType[] = await callSP({ procedure: STORED_PROCEDURES.GET_PAGE_IMAGE, values: [page] });
+        console.log(image)
         res.json(image[0]);
     }
     catch (error) {
